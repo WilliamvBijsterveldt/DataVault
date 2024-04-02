602cd7f17c886bc73d9b9a56abe88639f4756a92
WilliamvBijsterveldt <wvanbijsterveldt@gmail.com>
2024-04-02 19:36:57 +0200

    Added models + EF core

---
+++ DataVault.API/Data/UserDBContext.cs
@@ -0,0 +1,10 @@
+﻿using DataVault.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataVault.API.Data;
+
+public class UserDBContext: DbContext
+{
+    public UserDBContext(DbContextOptions options) : base(options) { }
+    public DbSet<User> User { get; set; }
+}
---
+++ DataVault.API/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace DataVault.API.Models;
+
+public class User
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public int Age { get; set; }
+}
